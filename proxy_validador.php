0825c8daf98696495e54582f65e8587eca8b2862
Walter Sadrac <92003012+Walt2dev@users.noreply.github.com>
2026-02-25 13:52:16 -0800

    Update proxy_validador.php

--- proxy_validador.php
+++ proxy_validador.php
@@ -1,19 +1,25 @@
 <?php
-// check_tunel.php
-$url = 'http://integranet.dyndns.org:81/cotizanet/signin/tx.php';
+$url = 'http://integranet.dyndns.org:81/cotizanet/signin/vaca.php';
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_TIMEOUT, 10); // Esperar máximo 10 segundos
+curl_setopt($ch, CURLOPT_VERBOSE, true);
+curl_setopt($ch, CURLOPT_HEADER, true); // ESTO ES LA CLAVE: Nos dará las cabeceras
 
-$result = curl_exec($ch);
-$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+$response = curl_exec($ch);
+$info = curl_getinfo($ch);
+
+echo "<h2>Diagnóstico de Conexión</h2>";
 
 if (curl_errno($ch)) {
-    echo "Error de cURL: " . curl_error($ch);
+    echo "<b>Error de Red:</b> " . curl_error($ch);
 } else {
-    echo "Código HTTP: " . $http_code . "<br>";
-    echo "Respuesta del Servidor Local: <b>" . $result . "</b>";
+    echo "<b>Código HTTP:</b> " . $info['http_code'] . "<br>";
+    echo "<b>Tipo de Contenido:</b> " . $info['content_type'] . "<br>";
+    echo "<b>Servidor que responde:</b> " . ($info['primary_ip'] ?? 'No detectada') . "<br>";
+    
+    echo "<h3>Cabeceras de respuesta (Aquí verás quién te bloquea):</h3>";
+    echo "<pre>" . htmlspecialchars($response) . "</pre>";
 }
 
 curl_close($ch);
