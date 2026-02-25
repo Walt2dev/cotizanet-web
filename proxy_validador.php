e87333c20908e2ca6ec24b59e757ab2ff1cd9a35
Walter Sadrac <92003012+Walt2dev@users.noreply.github.com>
2026-02-25 13:15:46 -0800

    Update proxy_validador.php

--- proxy_validador.php
+++ proxy_validador.php
@@ -1,30 +1,23 @@
 <?php
 // proxy_validador.php
-
-// 1. Configuración del destino (tu túnel)
 $url = 'http://integranet.dyndns.org:81/cotizanet/signin/valdiate_sesion2.php';
 
-// 2. Inicializar cURL para reenviar la petición POST
 $ch = curl_init($url);
 
-// Configurar cURL para que se comporte como un puente
+// Convertimos el array $_POST en una cadena de consulta (user=admin&pass=123)
+$postData = http_build_query($_POST);
+
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST); // Reenvía todo lo que llegó por POST (user y pass)
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postData); 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_TIMEOUT, 10); // Tiempo límite de espera
 
-// 3. Ejecutar y obtener respuesta
+// IMPORTANTE: Decirle al servidor local que esto es un formulario estándar
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/x-www-form-urlencoded'
+));
+
 $result = curl_exec($ch);
-$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-if (curl_errno($ch)) {
-    // Si el túnel está caído, avisamos al JS
-    echo "error_conexion";
-} else {
-    // Devolvemos lo que diga tu servidor local (success o fail)
-    echo $result;
-}
-
 curl_close($ch);
+
+echo $result;
 ?>
